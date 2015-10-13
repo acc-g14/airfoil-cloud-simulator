c8ab9cd2f8ab916e7249ebb132ad3471af78e714
Michael Müller <michael.mueller.90@gmail.com>
2015-10-13 19:33:12 +0200

    added default model classes, refs #6

---
+++ model/UserParameters.py
@@ -0,0 +1,17 @@
+
+
+class UserParameters:
+    """
+        Class holding parameters submitted by the user.
+    """
+
+    def __init__(self):
+        """
+            initialize variables
+        """
+        self.naca4 = None
+        self.minAngle = None
+        self.maxAngle = None
+        self.step = None
+        self.numNodes = None
+        self.refinementLevel = None
