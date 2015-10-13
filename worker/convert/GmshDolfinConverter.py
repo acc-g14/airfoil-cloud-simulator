9b4803c5f1a117d767d292e2743af6af77d823ce
Michael Müller <michael.mueller.90@gmail.com>
2015-10-13 20:13:47 +0200

    created basic worker stubs.

---
+++ worker/convert/GmshDolfinConverter.py
@@ -0,0 +1,10 @@
+from worker.convert.Converter import Converter
+
+
+class GmshDolfinConverter(Converter):
+    """
+        Class to convert from Gmsh format to dolfin xml format.
+    """
+
+    def convert(self, f):
+        pass
---
+++ worker/create/ModelCreator.py
@@ -0,0 +1,19 @@
+import abc
+
+
+class ModelCreator:
+    """
+        Interface to create a model.
+    """
+
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def create_model(self, params):
+        """
+        Method that creates a model based on the passed parameters
+        :param model.ComputeParameters.ComputeParameters params: ComputeParameters
+        :return:
+        """
+        pass
