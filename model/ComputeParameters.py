1f7f377e6ea075c9bf54fdf58aaa1aea995283fd
Adam Risberg <risbergadam@gmail.com>
2015-10-19 19:35:08 +0200

    Creating a model and doing the actual airfoil computations are two distinct processes, so it makes sense to split the paramterers of these processes

--- model/ComputeParameters.py
+++ model/ComputeParameters.py
@@ -2,19 +2,15 @@
 
 class ComputeParameters:
     """
-        Class holding the parameters for the computation.
+        Class holding the parameters for the computation using the airfoil binary.
     """
     def __init__(self):
         """
         Initialize instance variables (eventually with default values).
         :return: void
         """
-        self.naca4 = None
-        self.job = None
-        self.angle = None
-        self.num_nodes = None
-        self.refinement_level = None
-        self.navier_num_samples = None
-        self.navier_viscosity = None
-        self.navier_speed = None
-        self.navier_time = None
+        self.num_samples = None
+        self.viscosity = None
+        self.speed = None
+        self.time = None
+        self.xml_file = None
---
+++ model/ModelParameters.py
@@ -0,0 +1,16 @@
+
+class ModelParameters:
+    """
+        Class holding the parameters for creating and converting models.
+    """
+    def __init(self):
+        """
+        Initialize instance variables (eventually with default values).
+        :return: void
+        """
+        self.naca4 = None
+        self.job = None
+        self.angle = None
+        self.num_nodes = None
+        self.refinement_level = None
+        self.output_filename = None
