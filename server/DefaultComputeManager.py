fbae2cd30d382f3c9058ed8b65d14a15991957f9
Michael Müller <michael.mueller.90@gmail.com>
2015-10-19 22:14:13 +0200

    modified server structure, closes #16

--- server/DefaultComputeManager.py
+++ server/DefaultComputeManager.py
@@ -3,6 +3,9 @@ from ComputeManager import ComputeManager
 
 class DefaultComputeManager(ComputeManager):
 
+    def __init__(self, storage):
+        super(DefaultComputeManager, self).__init__(storage)
+
     def stop_computation(self, job):
         pass
 
@@ -12,5 +15,11 @@ class DefaultComputeManager(ComputeManager):
     def get_result(self, job):
         pass
 
-    def start_computation(self, params):
-        pass
+    def start_computation(self, model_params, compute_params):
+        """
+        This method starts the computation with the defined parameters.
+
+        :param model.ModelParameters.ModelParameters model_params: ModelParameters
+        :param model.ComputeParameters.ComputeParameters compute_params: ComputeParameters
+        :rtype : UUID
+        """
