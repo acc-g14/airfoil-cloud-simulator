107800db8cc67af94e41b08f491a7259f28eba6b
adam risberg <risbergadam@gmail.com>
2015-10-20 15:04:26 +0000

    variable names are now according to pep8

--- model/UserParameters.py
+++ model/UserParameters.py
@@ -10,11 +10,11 @@ class UserParameters:
             initialize variables
         """
         self.naca4 = [0, 0, 1, 2]
-        self.minAngle = 0
-        self.maxAngle = 90
+        self.min_angle = 0
+        self.max_angle = 90
         self.step = 10
-        self.numNodes = 200
-        self.refinementLevel = 0
+        self.num_nodes = 200
+        self.refinement_level = 0
         self.num_samples = 2
         self.viscosity = 0.0001
         self.speed = 10
