500f93c1839e5bd017ced53587d57a0ebe0e6d81
Michael Müller <michael.mueller.90@gmail.com>
2015-10-21 12:01:29 +0200

    fix

--- server/DefaultWorkerManager.py
+++ server/DefaultWorkerManager.py
@@ -64,7 +64,6 @@ class DefaultWorkerManager(WorkerManager):
 
     @staticmethod
     def _init_worker(workerip):
-        try:
             ssh = paramiko.SSHClient()
             ssh.load_system_host_keys()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
