d8c1dcd6455a1ebf24710d5264900bd13ce320ac
Michael Müller <michael.mueller.90@gmail.com>
2015-10-22 15:58:53 +0200

    fixed worker error

--- workertasks.py
+++ workertasks.py
@@ -5,6 +5,8 @@ from worker.compute.AirfoilComputation import AirfoilComputation
 from worker.convert.GmshDolfinConverter import GmshDolfinConverter
 from worker.create.GmshModelCreator import GmshModelCreator
 from storage.Storage import Storage
+import model.ModelParameters
+import model.UserParameters
 import os
 import pycurl
 
