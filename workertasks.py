3a3ee65d149d6a2d6cc4d8d3b55a32c9361789db
Adam Risberg <risbergadam@gmail.com>
2015-10-22 09:36:39 +0000

    each task will now get its own working directory in workdir

--- workertasks.py
+++ workertasks.py
@@ -5,6 +5,7 @@ from worker.convert.GmshDolfinConverter import GmshDolfinConverter
 from worker.create.GmshModelCreator import GmshModelCreator
 from model.ModelParameters import ModelParameters
 from model.ComputeParameters import ComputeParameters
+import os
 
 app = Celery("CloudProjectWorker", backend="amqp://", broker="amqp://")
 
@@ -19,8 +20,16 @@ def simulate_airfoil(modelParams, computeParams):
     :param model.ModelParameters.ModelParameters modelParams: ModelParameters
     :param model.ComputeParameters.ComputeParameters computeParams: ComputeParameters
     """
+    root_dir = os.getcwd()
+    working_dir = root_dir + "/workdir/" + str(modelParams.job) + "/a" + str(modelParams.angle)
+    if not os.path.exists(working_dir): os.makedirs(working_dir)
+    os.chdir(working_dir)
+
     msh_file = creator.create_model(modelParams)
     xml_file = converter.convert(msh_file)
     result = computation.perform_computation(computeParams, xml_file)
     result['angle'] = modelParams.angle
+
+    os.chdir(root_dir)
+
     return result
