4133b2f2d441c21146e8a025576b1836a8092f21
Michael Müller <michael.mueller.90@gmail.com>
2015-10-22 14:25:41 +0200

    use cloud-init

--- server/DefaultWorkerManager.py
+++ server/DefaultWorkerManager.py
@@ -1,7 +1,6 @@
 from WorkerManager import WorkerManager
 from novaclient.client import Client
 from netifaces import interfaces, ifaddresses, AF_INET
-import paramiko
 
 
 class DefaultWorkerManager(WorkerManager):
@@ -36,54 +35,13 @@ class DefaultWorkerManager(WorkerManager):
         # basic parameters
         image = self.nc.images.find(name="G14Worker")
         flavor = self.nc.flavors.find(name="m1.medium")
-        servers_to_init = []
-        servers_to_start = []
+        cloud_init = "#!/bin/bash \n" + \
+                     " cd /home/ubuntu/airfoil-cloud-simulator \n" + \
+                     " su -c 'celery -A workertasks worker -b amqp://cloudworker:worker@" + \
+                     self._my_ip() + "//' ubuntu"
         for i in range(0, num):
-            server = self.nc.servers.create("G14Worker" + str(i), image, flavor, key_name="G14Key")
-            servers_to_init.append(server)
-        # loop as long as there are still servers to init/start
-        while len(servers_to_init) > 0 or len(servers_to_start) > 0:
-            still_to_init = []
-            still_to_start = []
-            for server in servers_to_init:
-                if server.status == "BUILD":
-                    still_to_init.append(self.nc.servers.get(server))
-                else:
-                    # get ip and associate it with server
-                    print "A Server finished Build. Status: {0}".format(str(server.status))
-                    ip = None
-                    for key, network in server.networks.iteritems():
-                        ip = network[0]
-                        break
-                    servers_to_start.append({"server": server, "ip": ip})
-            for server in servers_to_start:
-                # perform some operations via ssh on worker
-                init_successful = self._init_worker(server["ip"])
-                if not init_successful:
-                    # init could not be performed
-                    still_to_start.append(server)
-                else:
-                    # worker fully initialized
-                    self._workers.append(server["server"])
-            servers_to_init = still_to_init
-            servers_to_start = still_to_start
-
-    @staticmethod
-    def _init_worker(workerip):
-        try:
-            ssh = paramiko.SSHClient()
-            ssh.load_system_host_keys()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(str(workerip), username="ubuntu")
-            session = ssh.get_transport().open_session()
-            session.exec_command(
-                "cd airfoil-cloud-simulator/ && " +
-                "screen -d -m celery worker -A workertasks -b amqp://cloudworker:worker@" +
-                DefaultWorkerManager._my_ip() + "//")
-            ssh.close()
-            return True
-        except:
-            return False
+            server = self.nc.servers.create("G14Worker" + str(i), image, flavor, userdata=cloud_init)
+            self._workers.append(server)
 
     # from http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
     @staticmethod
