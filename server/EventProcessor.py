78dd8a82d61e10c41fcf44ceecc423a822ac70c9
Michael Müller <michael.mueller.90@gmail.com>
2015-10-31 20:22:35 +0100

    fix detection of inactive workers

--- server/EventProcessor.py
+++ server/EventProcessor.py
@@ -32,7 +32,7 @@ class EventProcessor:
         if result is not None:
             starttime = init_time - result[0]
             print "STARTTIME: " + str(starttime)
-            DBUtil.execute_command(self._config.db_name, "UPDATE Workers SET initialized = 'true', starttime = ?, heartbeat = ?  WHERE name = ? ", (starttime, time.time(), name))
+            DBUtil.execute_command(self._config.db_name, "UPDATE Workers SET initialized = 'true', starttime = ?, heartbeat = ?, active = ?  WHERE name = ? ", (starttime, time.time(), time.time(), name))
 
     def worker_heartbeat(self, event):
         """
@@ -41,7 +41,6 @@ class EventProcessor:
         """
         hostname = event['hostname']
         if "active" in event:
-            print "active found"
             self._update_worker(hostname, event['active'])
         else:
             self._update_worker(hostname)
